dc02181c42334f24aed4327919d8905146e9e292
Eleyine Zarour (Ubuntu) <eleyine@users.noreply.github.com>
2022-08-25 05:23:14 -0400

    update scripts to include conda env

--- scripts/finetune_no_SSL.sh
+++ scripts/finetune_no_SSL.sh
@@ -1,3 +1,3 @@
-
-python /root/self-supervised-3d-tasks/self_supervised_3d_tasks/train.py /root/self-supervised-3d-tasks/self_supervised_3d_tasks/configs/finetune/do_cpc_2d_rsna_ich_with_SSL.json
+#!/bin/bash
+/root/anaconda3/envs/conda-env/bin/python /root/self-supervised-3d-tasks/self_supervised_3d_tasks/train.py /root/self-supervised-3d-tasks/self_supervised_3d_tasks/configs/finetune/do_cpc_2d_rsna_ich_with_SSL.json
 
