bfeff05a38ec9d66dac474ca3dbf0ba03c2df3b4
Eleyine Zarour (Ubuntu) <eleyine@users.noreply.github.com>
2022-08-25 05:25:58 -0400

    fix command

--- scripts/finetune_no_SSL.sh
+++ scripts/finetune_no_SSL.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/root/anaconda3/envs/conda-env/bin/python /root/self-supervised-3d-tasks/self_supervised_3d_tasks/train.py /root/self-supervised-3d-tasks/self_supervised_3d_tasks/configs/finetune/do_cpc_2d_rsna_ich_with_SSL.json
+/root/anaconda3/envs/conda-env/bin/python /root/self-supervised-3d-tasks/self_supervised_3d_tasks/finetune.py /root/self-supervised-3d-tasks/self_supervised_3d_tasks/configs/finetune/do_cpc_2d_rsna_ich_with_SSL.json
 
